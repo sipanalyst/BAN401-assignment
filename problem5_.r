15699a4f168432f5f1968eff117f261a82f92ad9
Sipan Sat <sipansat@MacBook-Air-von-Sipan.local>
2024-11-03 21:27:27 +0100

    Another improvement

--- problem5_.r
+++ problem5_.r
@@ -1,47 +1,59 @@
 # BAN401 - Applied Programming and Data Analysis for Business
 
-# Explanation of problem:
-# 
-
-
 #------------------------- Solution of Problem 5
 
 
 items_df <-
-  tibble::tibble(
+  tibble::tibble( #we access to tibble--library trough tibble:: and extract command 
+                  #tibble() in order to create a modern data frame. 
   items = c("small item", "medium item", "large item", "extra-large item",
            "luxury item", "premium item", "bulk pack"),
+        #->first column contains the given items by creating a vector with character 
+        #. elements.
   costs = c(1,2,5,10,20,50,100)
+        #->second column contains corresponding costs of items in dollar by creating
+        #. a vector of numbers with data tyoe double as default.
 )
 
 
-# Target amount
-target_amount <- 200
-
-# Recursive function:
+#recursive function:
 count_combinations <- function(target, costs, index) {
-  # Base cases
-  # (1) found valid combinations where the sum of item costs is exactly equal to 200:
+  #base cases
+  #(1) found valid combinations where the sum of item costs is exactly equal to 200:
   if (target == 0) 
     return(1)       
-  # (2) found not valid combinations where the sum of item costs is higher than 
-  #.    200 or there are no more items left :
+  #(2) found not valid combinations where the sum of item costs is higher than 
+  #.   200 or there are no more items left :
   if (target < 0 | index < 1) 
     return(0)
   
-  # Recursive calls: include or exclude the current item
-  include_current_item <- count_combinations(target - costs[index], costs, index)
-  exclude_current_item <- count_combinations(target, costs, index - 1)
+  #recursive syntax: include or exclude the current item
+  include_current_item <- count_combinations(target - costs[index],
+                                             costs,
+                                             index)
+  #-> if item is included then cost of item will deduct the target spend volume
+  exclude_current_item <- count_combinations(target,
+                                             costs,
+                                             index - 1)
+  #->if item is excluded, then index will deduct in order to change the item
   
-  # Total ways is the sum of including and excluding the current item
+  #total combination paths is the sum of including and excluding the current item:
   return(as.numeric(include_current_item + exclude_current_item))
 }
 
-# Calculate the number of combinations without memoization
-total_combination_number <- count_combinations(target_amount, items_df$costs, length(items_df$costs))
 
-# Print the result
-cat("How many item combinations add up to $200?", "\n", "-> There are", total_combination_number, "unique item purchase combinations.")
+#calculate the number of combinations and store the value to the object
+# total_combination_number:
+total_combination_number <- count_combinations(target =200,
+                                               costs = items_df$costs, 
+                                               index = length(items_df$costs))
+
+#->here we use the self-defined count_combinations()-function and provide
+#. starting values to the arguments
+
+#print the result:
+cat("How many item combinations add up to $200?", "\n", "-> There are", 
+    total_combination_number, "unique item purchase combinations.")
 
 
 
--- problem_4.r
+++ problem_4.r
@@ -1,106 +1,73 @@
 # BAN401 - Applied Programming and Data Analysis for Business
 
-# Explanation of problem:
-# 
-library(magrittr) # in order to use pipe-operator
-library(dplyr) # in order to use efficient and time saving code structure 
-
-
 #------------------------- Solution of Problem 4
 
-#-------------------------------------------------------------------------------
-# Approach 2:
 #------------ a)
-routes_df <- 
-  tibble::tibble(
+routes_df <- #the created tibble is stored in object routes_df.
+  tibble::tibble( #we create access to tibble library through tibble:: and use 
+                  #the command tibble() in order to create a modern data frame 
+                  #object.
     route = c("City A -> B", "City B -> C", "City A -> C"),
+    #first column "route" contains route, it can be interpreted as unique identifier.
     distance = c(350, 500, 750),
+    #second column "distance"contains the specific distances in km for each route.
     ferries = c(1, 2, 3),
+    #third column "ferries" contains number of ferries for each route.
     tolls = c(3, 5, 6)
+    #fourth column "tolls" contains number of toll stations for each route.
   )
 
-vehicle_type_cost_list <-
-  list(
-    ET = c("Energy cost per km" = 0.2,
+vehicle_type_cost_list <-#the created list is stored in vehicle_type_cost_list
+  list(# list() is a command to create a list that contains plenty possible objects 
+       #which do not necessarily have to be the same data type.
+    ET = c("Energy cost per km" = 0.2, 
            "Price per ferry ride" = 180,
            "Toll cost per station" = 40),
+    #->ET (Electric Trucks) is defined as the first object in the list.
+    #->it contains three elements. 
     DT = c("Fuel cost per km" = 1.5,
            "Price per ferry ride" = 300,
            "Toll cost per station" = 80)
+    #->DT (Diesel Trucks) is defined as the second object in the list.
+    #->it contains three elements.
   )
 
-# Calculation of total costs:
-
-# (1) ET 
-et_route_ab <- as.double(vehicle_type_cost_list[[1]][1] * routes_df$distance[1] + 
-                            vehicle_type_cost_list[[1]][2] * routes_df$ferries[1] +
-                            vehicle_type_cost_list[[1]][3] * routes_df$tolls[1])
-
-
-
-et_route_bc <- as.double(vehicle_type_cost_list[[1]][1] * routes_df$distance[2] +
-                            vehicle_type_cost_list[[1]][2] * routes_df$ferries[2] +
-                            vehicle_type_cost_list[[1]][3] * routes_df$tolls[2])
-
-et_route_ac <- as.double(vehicle_type_cost_list[[1]][1] * routes_df$distance[3] +
-                            vehicle_type_cost_list[[1]][2] * routes_df$ferries[3] +
-                            vehicle_type_cost_list[[1]][3] * routes_df$tolls[3])
-
-# (2)DT
-
-dt_route_ab <- as.double(vehicle_type_cost_list[[2]][1] * routes_df$distance[1] +
-                            vehicle_type_cost_list[[2]][2] * routes_df$ferries[1] +
-                            vehicle_type_cost_list[[2]][3] * routes_df$tolls[1])
-
-dt_route_bc <- as.double(vehicle_type_cost_list[[2]][1] * routes_df$distance[2] +
-                            vehicle_type_cost_list[[2]][2] * routes_df$ferries[2] +
-                            vehicle_type_cost_list[[2]][3] * routes_df$tolls[2])
-
-dt_route_ac <- as.double(vehicle_type_cost_list[[2]][1] * routes_df$distance[3] +
-                            vehicle_type_cost_list[[2]][2] * routes_df$ferries[3] +
-                            vehicle_type_cost_list[[2]][3] * routes_df$tolls[3])
-
-routes_df$et_total <- c(et_route_ab, et_route_bc, et_route_ac)
-routes_df$dt_total <- c(dt_route_ab, dt_route_bc, dt_route_ac)
-
-routes_df <- routes_df %>%
-  mutate(et_total = c(et_route_ab, et_route_bc, et_route_ac),
-         dt_total = c(dt_route_ab, dt_route_bc, dt_route_ac))
-
-
-#------------ b)
-routes_df <- routes_df %>%
-  mutate(savings = dt_total - et_total)
-
-
-
-
-
-
-
-
-
-#-------------------------------------------------------------------------------
-# Approach 2:
 #------------ a)
-vehicle_type_cost_df <- as.data.frame(vehicle_type_cost_list)
+
 
 et_total <- vector(mode = "double", length = 3)
 dt_total <- vector(mode = "double", length = 3)
+#->we create two empty vectors with data type double and a length of three in order
+#  to receive a vector which can store the explicit vales we create in the upcoming 
+#. for-loop. These vectors are named et_total (Total transportation costs of 
+#. electric trucks) and dt_total (Total transportation costs of diesel trucks).
 
-for(i in 1:nrow(routes_df)) {
-  et_total[i] <- sum(routes_df[i,c(2,3,4)] * vehicle_type_cost_df$ET)
-  dt_total[i] <- sum(routes_df[i,c(2,3,4)] * vehicle_type_cost_df$DT)
+for(i in 1:nrow(routes_df)) {#iterate i from one to three which is the number of 
+                             #rows in routes_df and therefore the number of routes.
+  et_total[i] <- sum(routes_df[i,c(2,3,4)] * vehicle_type_cost_list$ET)
+  #-> multiplication of the values of routes_df for each row and all numerical 
+  #.  values with the corresponding elements in object ET of the truck cost list.
+  #-> iterative process, that means first route is stored as first item in et_total
+  #.  up until the last route.
+  dt_total[i] <- sum(routes_df[i,c(2,3,4)] * vehicle_type_cost_list$DT)
+  #-> multiplication of the values of routes_df for each row and all numerical 
+  #.  values with the corresponding elements in object DT of the truck cost list.
+  #-> iterative process, that means first route is stored as first item in dt_total
+  #.  up until the last route.
 }
 
-routes_df$et_total <- et_total
+routes_df$et_total <- et_total 
+#->create new column in routes_df called et_total which stores et_total created above. 
 routes_df$dt_total <- dt_total
-
+#->create new column in routes_df called dt_total which stores dt_total created above. 
+routes_df
 #------------ b)
 routes_df$savings <- routes_df$dt_total - routes_df$et_total
-
+#->create new column named savings which is created by the total transportation
+#. costs of diesel trucks minus the total transportation costs of electric cars for 
+#. each route.
 routes_df
-
+#->final tibble
 
 
 
